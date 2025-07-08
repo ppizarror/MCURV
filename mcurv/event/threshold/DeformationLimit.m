81b43926b3ca9cdd40e9ae5ee9d10abd6d8b1fd8
ppizarror <pablo@ppizarror.com>
2025-07-08 14:38:28 -0400

    Agrega evento def maximo y abs

--- mcurv/event/threshold/DeformationLimit.m
+++ mcurv/event/threshold/DeformationLimit.m
@@ -25,30 +25,50 @@
 classdef DeformationLimit < GenericEvent
 
     properties (Access = protected)
+        epsLim % Maxima deformacion registrada
         hit % Umbral superado
         hitData % Valor de [phix, phiy, mx, my] que logra deformacion
+        isAbs % Usa valor absoluto
         maxEps % Maxima deformacion
         name % Nombre evento
     end % protected properties
 
     methods (Access = public)
 
-        function obj = DeformationLimit(eventName, maxEps)
+        function obj = DeformationLimit(eventName, maxEps, isAbs)
             % DeformationLimit: Constructor de la clase
             %
             % Parametros:
             %   maxEps     Maxima deformacion
-
-            if nargin ~= 2
+            %   abs        Usa valor absoluto. Verdadero por defecto
+            if nargin < 3
+                isAbs = false;
+            end
+            if nargin < 2
                 error('Numero de parametros incorrectos, uso: %s', ...
-                    'DeformationLimit(eventName,max_eps)');
+                    'DeformationLimit(eventName,max_eps,abs)');
             end
             obj = obj@GenericEvent(eventName);
-            obj.maxEps = maxEps;
+            obj.epsLim = [0, 0, 0];
+            obj.isAbs = isAbs;
+            if isAbs
+                obj.maxEps = abs(maxEps);
+            else
+                obj.maxEps = maxEps;
+            end
             obj.name = eventName;
             obj.reset();
 
         end % DeformationLimit constructor
+
+        function [e, phix, phiy] = eLim(obj)
+            % eLim: Retorna la maxima deformacion registrada (def, phix, phiy)
+
+            e = obj.epsLim(1);
+            phix = obj.epsLim(2);
+            phiy = obj.epsLim(3);
+
+        end % eLim function
 
         function o = phix(obj)
             % phix: Deformacion si se cumple condicion evento
@@ -56,7 +76,7 @@ classdef DeformationLimit < GenericEvent
             o = obj.hitData(1);
 
         end % phix function
-        
+
         function o = phiy(obj)
             % phiy: Deformacion si se cumple condicion evento
 
@@ -66,10 +86,12 @@ classdef DeformationLimit < GenericEvent
 
         function eval(obj, e0, phix, phiy, eps, f, E, p, mx, my, n)
             % eval: Evalua el evento
-
+            if abs(eps) > abs(obj.epsLim(1))
+                obj.epsLim = [eps, phix, phiy];
+            end
             if obj.hit
                 return
-            elseif obj.maxEps < 0 && eps < obj.maxEps || obj.maxEps > 0 && eps > obj.maxEps
+            elseif (obj.isAbs && abs(eps) > obj.maxEps) || (obj.maxEps < 0 && eps < obj.maxEps || obj.maxEps > 0 && eps > obj.maxEps)
                 fprintf('\n');
                 dispMCURV();
                 if obj.maxEps < 0
@@ -95,10 +117,10 @@ classdef DeformationLimit < GenericEvent
             dispMCURV();
 
         end % disp function
-        
+
         function reset(obj)
             % reset: Resetea evento
-            
+
             obj.hit = false;
             obj.hitData = [0, 0, 0, 0];
 
